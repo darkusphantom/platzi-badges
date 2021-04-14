f02a5b6bf4ab6bd002362263e9929cb81abe6c48
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-04-14 18:59:17 -0400

    ADD: Añadiendo la pagina de BadgeNew
    Se creo e implemento los estilos, el navbar y el componente Badge a la pagina de BadgeNew

--- src/index.js
+++ src/index.js
@@ -3,19 +3,8 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./global.css";
-//import Badge from "./components/Badge";
-//import PhotoProfile from "./images/photo.jpg";
 import BadgeNew from "./pages/BadgeNew";
 
 const container = document.getElementById("app");
-/*
-  <Badge
-    firstName="Luis"
-    lastName="Rodriguez"
-    jobTitle="FrontEnd Developer"
-    user_twitter="WitcherPhantom"
-    url_avatar={PhotoProfile}
-  />,
-  */
 
 ReactDOM.render(<BadgeNew />, container);
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,12 +1,33 @@
 import React from "react";
 
+import "./styles/BadgeNew.css";
 import NavBar from "../components/NavBar";
+import header from "../images/badge-header.svg";
+
+import Badge from "../components/Badge";
+import PhotoProfile from "../images/photo.jpg";
 
 class BadgeNew extends React.Component {
   render() {
     return (
       <div>
         <NavBar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstName="Luis"
+                lastName="Rodriguez"
+                jobTitle="FrontEnd Developer"
+                user_twitter="WitcherPhantom"
+                url_avatar={PhotoProfile}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
