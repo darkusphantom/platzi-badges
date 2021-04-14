5e3c3099558169f8a2743af282aedaeaeedfbfd6
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-04-14 15:13:16 -0400

    ADD: Añadi una foto al badge
    Coloque una foto en la carpeta de images en la ruta de src y le agregue esa direccion a ignorar
    para .gitignore

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./global.css";
 import Badge from "./components/Badge";
+import PhotoProfile from "./images/photo.jpg";
 
 const container = document.getElementById("app");
 
@@ -13,6 +14,7 @@ ReactDOM.render(
     lastName="Rodriguez"
     jobTitle="FrontEnd Developer"
     user_twitter="WitcherPhantom"
+    url_avatar={PhotoProfile}
   />,
   container
 );
